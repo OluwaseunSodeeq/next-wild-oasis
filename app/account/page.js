7ce8c858c926a9bdc93e709608baeca8b7865d94
Oluwaseun Sodeeq <ademolaoluwaseun90@gmail.com>
2024-08-27 22:46:19 +0100

    default styling achieved

---
+++ app/account/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function Page() {
+  return (
+    <>
+      <h1>Account!</h1>
+      <Link href="/about">About</Link>;
+    </>
+  );
+}
+
+export default Page;
---
+++ app/account/reservations/page.js
@@ -0,0 +1,25 @@
+import { auth } from "../_lib/auth";
+
+export const metadata = {
+  title: "Guest area",
+};
+
+export default async function Page() {
+  const session = await auth();
+
+  const firstName = session.user.name.split(" ").at(0);
+
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {firstName}
+    </h2>
+  );
+}
+/*
+import Spinner from "@/app/_components/Spinner";
+
+export default function Loading() {
+  return <Spinner />;
+}
+
+*/
